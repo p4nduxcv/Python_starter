958e77be102a22f718cf5ba2eee4003838a41d57
Pandu <pandula95@gmail.com>
2025-04-21 11:30:28 +0530

    Replacing Blanks with guessess

--- Day 7/main.py
+++ Day 7/main.py
@@ -1,1 +1,22 @@
 
+import random
+word_list = ["aardvark", "baboon", "camel"]
+
+random.shuffle(word_list)
+word = word_list[0].lower()
+print(word)
+placeholder = ""
+display = ""
+
+for letter in range(1, len(word) + 1):
+    placeholder += "_"
+print(placeholder)
+
+guessed_letter = input("Enter Letter: ")
+
+for letter in word:
+    if letter == guessed_letter:
+        display += guessed_letter
+    else:
+        display += "_"
+print(display)
