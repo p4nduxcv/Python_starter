0a100e4fc3014fd37ea91154e66d52a5749ba0b6
Pandu <pandula95@gmail.com>
2025-04-23 12:02:14 +0530

    Keeping track of the player's Lives

--- Day 7/main.py
+++ Day 7/main.py
@@ -7,18 +7,27 @@ print(word)
 placeholder = ""
 isGameOver = False
 display = ["_"] * len(word)
+lives = 6
 
 print("".join(display))
 
 while not isGameOver:
+    print(f"You have {lives} lives left.")
     guessed_letter = input("Guess a letter: ").lower()
     for index, letter in enumerate(word):
         if letter == guessed_letter:
             display[index] = letter
+
     print("".join(display))
 
     if "_" not in display:
         print("You win!")
+        isGameOver = True
+
+    lives -= 1
+
+    if lives == 0 and "_" in display:
+        print("You lose!")
         isGameOver = True
 
 
