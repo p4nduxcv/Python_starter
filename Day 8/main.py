71490e861749653207a58a7ff676a493b59ed9bb
Pandu <pandula95@gmail.com>
2025-04-28 05:47:21 +0530

    Python Dictionaries

--- Day 8/main.py
+++ Day 8/main.py
@@ -1,51 +1,11 @@
-import string
-from collections import deque
+capitals = {
+    "France":"Paris",
+    "Germany":"Berlin"
+}
 
-direction = input("Encode or Decode?").lower()
-text7 = input("Enter Your Message:").lower()
-shift_amount = int(input("Enter Shift Amount:"))
+Travel_log = {
+    "Sri Lanka":["Colombo", "Kurunegala"],
+    "Germany":["Berlin", "Frukfurt"]
+}
 
-
-
-def encrypt(original_text, shift_amount):
-    original_text_array = list(original_text)
-    alphabet = list(string.ascii_lowercase)
-    d = deque(alphabet)
-    d.rotate(shift_amount)
-    rotated_alphabet = list(d)
-    encrypted_text = ""
-
-    for letter in original_text_array:
-        if letter in alphabet:
-            index = alphabet.index(letter)
-            encrypted_text += rotated_alphabet[index]
-        else:
-            # If not a letter (maybe a space, number, etc.), keep it as is
-            encrypted_text += letter
-    print(encrypted_text)
-
-def decript(original_text, shift_amount):
-    original_text_array = list(original_text)
-    alphabet = list(string.ascii_lowercase)
-    d = deque(alphabet)
-    d.rotate(-shift_amount)
-    rotated_alphabet = list(d)
-    decrypted_text = ""
-
-    for letter in original_text_array:
-        if letter in alphabet:
-            index = alphabet.index(letter)
-            print(index)
-            decrypted_text += rotated_alphabet[index]
-        else:
-
-            decrypted_text += letter
-            print(decrypted_text)
-
-    print(decrypted_text)
-
-decript(text7, shift_amount)
-
-
-
-
+print(Travel_log["Germany"])
