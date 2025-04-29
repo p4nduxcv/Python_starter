0c0d5d6f4e22921151861afa231dd6bc8b6e3376
Pandu <pandula95@gmail.com>
2025-04-29 13:08:50 +0530

    The calculator

--- Day 10/main.py
+++ Day 10/main.py
@@ -1,5 +1,36 @@
-def format_name(f_name, l_name):
-    formated_name = f"{f_name} {l_name}"
-    return formated_name.title()
+def add (a, b):
+    return a + b
+def subtract (a, b):
+    return a - b
+def multiply (a, b):
+    return a * b
+def divide (a, b):
+    return a / b
 
-print(format_name("joNn", "doe"))
+operations_dict = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+is_deal_final_value = True
+
+while is_deal_final_value:
+    value_1 = int(input("Enter first number: "))
+    value_2 = int(input("Enter second number: "))
+    operation = input("Enter operation: + - * / ")
+    final_value = operations_dict[operation](value_1,value_2)
+    print(final_value)
+
+    is_deal_final_value = bool(input("Enter a boolean value (True or False): "))
+
+    if is_deal_final_value:
+        value_2 = int(input("Enter second number: "))
+        operation = input("Enter operation: + - * / ")
+        final_value = operations_dict[operation](final_value, value_2)
+        print(final_value)
+        is_deal_final_value = bool(input("Enter a boolean value (True or False): "))
+    else:
+        is_deal_final_value = False
+
