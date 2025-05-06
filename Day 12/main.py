997f456468e5501a602cd556c14eff6dc267097f
Pandu <pandula95@gmail.com>
2025-05-06 06:00:17 +0530

    Number Guessing Game Complete

--- Day 12/main.py
+++ Day 12/main.py
@@ -5,17 +5,36 @@ EASY_LEVEL_GUESSES = 10
 HARD_LEVEL_GUESSES = 5
 
 
+def set_difficulty(difficulty):
+    if difficulty == "easy":
+        return EASY_LEVEL_GUESSES
+    else:
+        return HARD_LEVEL_GUESSES
+
 
 def game():
     print(logo)
     random_number = random.randint(1, 100)
     difficulty = input("Choose a difficulty. Type 'easy' or 'hard':").lower()
+    turns = set_difficulty(difficulty)
 
+    print(turns)
 
-    if difficulty == "easy":
-        print(random_number)
-        print(f"You have {EASY_LEVEL_GUESSES} attempts remaining to guess the number.")
+    while turns > 0:
+        guessed_number = int(input(f"Guess a number between 1 and 100. You have {turns} attempts left: "))
+        if guessed_number == random_number:
+            print("You win!")
+            break
+        elif guessed_number > random_number:
+            print("Too high.")
+            turns -= 1
+        elif guessed_number < random_number:
+            print("Too low.")
+            turns -= 1
 
+        if turns == 0:
+            print("You have out of attempts. You lose.")
+            return
 
 
 
