1791ac4ab383eaf7848d488fc3e1386fa40e3398
Pandu <pandula95@gmail.com>
2025-05-19 08:39:52 +0530

    OOP in python

---
+++ Day 16/main.py
@@ -0,0 +1,11 @@
+from turtle import Turtle, Screen
+
+ranil = Turtle()
+UNP_Screen = Screen()
+
+ranil.shape("turtle")
+ranil.color("green")
+ranil.forward(100)
+
+print(UNP_Screen.canvheight)
+UNP_Screen.exitonclick()
