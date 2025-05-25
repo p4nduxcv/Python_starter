b6ea951adda5e9221aa67a7a91ee97159c302501
Pandu <pandula95@gmail.com>
2025-05-25 21:14:25 +0530

    Adding Methods and classess

--- Day 17/main.py
+++ Day 17/main.py
@@ -1,9 +1,19 @@
 class User:
 
-    def __init__(self, user_id):
+    def __init__(self, user_id, user_name):
         self.id = user_id
+        self.username = user_name
+        self.followers = 0
+        self.following = 0
 
+    def follow(self, user):
+        user.followers += 1
+        self.following += 1
 
-user_1 = User(100)
+user_1 = User(100, "Pandula")
+user_2 = User(101, "Max")
 
-print(user_1.id)
+user_1.follow(user_2)
+
+print(user_1.followers)
+print(user_1.following)
