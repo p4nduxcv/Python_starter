3ed2327af5657fb9e81f988c7f4bbf1ab140d70f
Pandu <pandula95@gmail.com>
2025-05-28 07:19:08 +0530

    Turtle 1st challenge

---
+++ Day 18/main.py
@@ -0,0 +1,13 @@
+from turtle import Turtle, Screen
+
+timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+
+
+for _ in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.left(90)
+
+
+timmy_the_turtle_screen = Screen()
+timmy_the_turtle_screen.exitonclick()
