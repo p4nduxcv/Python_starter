7e36f12e82a8e99763cc4bee3dff93d834c1b8cf
Pandu <pandula95@gmail.com>
2025-05-28 22:59:11 +0530

    Turtle random shape generator

--- Day 18/main.py
+++ Day 18/main.py
@@ -1,13 +1,27 @@
 from turtle import Turtle, Screen
+import random
 
 timmy_the_turtle = Turtle()
 timmy_the_turtle.shape("turtle")
+timmy_the_turtle.speed(2)
+colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple']
+
+def draw_shape(leg_count):
+    angle = 360/leg_count
+    for _ in range(leg_count):
+        timmy_the_turtle.forward(100)
+        timmy_the_turtle.left(angle)
 
 
-for _ in range(4):
-    timmy_the_turtle.forward(100)
-    timmy_the_turtle.left(90)
+#Squar
+for num_of_legs in range(3, 10):
+    timmy_the_turtle.color(random.choice(colors))
+    draw_shape(num_of_legs)
+
 
 
 timmy_the_turtle_screen = Screen()
 timmy_the_turtle_screen.exitonclick()
+
+
+
