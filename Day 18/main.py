bfea035c74c484c61b55289c9d6d2c7ab181c691
Pandu <pandula95@gmail.com>
2025-05-31 23:50:27 +0530

    Turtle random steps and RGB colors

--- Day 18/main.py
+++ Day 18/main.py
@@ -7,13 +7,19 @@ timmy_the_turtle.shape("turtle")
 timmy_the_turtle.speed(2)
 timmy_the_turtle.width(5)
 
-
 dir = [0, 90, 180, 270]
+
+def get_random_color():
+    r = random.randint(0,255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    random_color = (r,g,b)
+    return random_color
 
 for _ in range(200):
     timmy_the_turtle.forward(50)
     timmy_the_turtle.left(random.choice(dir))
-    timmy_the_turtle.color(random.choice(ColorsList))
+    timmy_the_turtle.pencolor(get_random_color())
 
 
 timmy_the_turtle_screen = Screen()
