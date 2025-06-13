b80937c7a1912a57d325ef968584618b2a48682e
Pandu <pandula95@gmail.com>
2025-06-13 13:20:50 +0530

    Create colorgram

--- Day 18/Hirst Painting/main.py
+++ Day 18/Hirst Painting/main.py
@@ -14,9 +14,30 @@ for color in colors:
 
 turtle_module.colormode(255)
 tim = turtle_module.Turtle()
+tim.speed("fastest")
+tim.penup()
+tim.hideturtle()
+
 tim.setheading(255)
 tim.forward(300)
+tim.setheading(0)
 tim.dot(20,random.choice(rgb_colors))
+number_of_dots = 100
+
+for dot_count in range(1, number_of_dots + 1):
+    tim.dot(20, random.choice(rgb_colors))
+    tim.forward(50)
+
+    if dot_count % 10 == 0:
+        tim.setheading(90)
+        tim.forward(50)
+        tim.setheading(180)
+        tim.forward(500)
+        tim.setheading(0)
+
+
+
 
 screen = turtle_module.Screen()
 screen.exitonclick()
+
