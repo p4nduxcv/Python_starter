0af50f79c5c1c8148645955eb2f151dca991dee4
Pandu <pandula95@gmail.com>
2025-06-15 00:52:32 +0530

    Etch A sketch App Done

--- Day 19/main.py
+++ Day 19/main.py
@@ -7,8 +7,26 @@ screen = Screen()
 def move_forward():
     tim.forward(10)
 
+def move_right():
+    new_heading = tim.heading() - 10
+    tim.setheading(new_heading)
+
+def move_left():
+    new_heading = tim.heading() + 10
+    tim.setheading(new_heading)
+
+def move_backward():
+    tim.backward(10)
+
+def clean():
+    tim.clear()
+
 screen.listen()
-screen.onkey(key="space", fun=move_forward)
+screen.onkey(move_forward,"w")
+screen.onkey(move_right,"d")
+screen.onkey(move_left,"a")
+screen.onkey(move_backward,"s")
+screen.onkey(clean,"c")
 screen.exitonclick()
 
 
