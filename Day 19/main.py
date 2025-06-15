dd9cadd72d29472ed250b7280da4111aa5369550
Pandu <pandula95@gmail.com>
2025-06-15 15:56:59 +0530

    Turtle race completed

--- Day 19/main.py
+++ Day 19/main.py
@@ -2,6 +2,7 @@ import random
 from turtle import Turtle, Screen
 
 
+is_race_start = False
 screen = Screen()
 screen.setup(500,600)
 user_bet = screen.textinput("Make your bet", "Which turtle will win the game ?")
@@ -13,11 +14,23 @@ for i in range(6):
     tim = Turtle(shape="turtle")
     tim.color(colors[i])
     turtles.append(tim)
-
-
-for i in range(0, len(turtles)):
     turtles[i].penup()
     turtles[i].goto(-250,-200 + i*100)
+
+if user_bet:
+    is_race_start = True
+
+while is_race_start:
+    for i in range(len(turtles)):
+        if turtles[i].xcor() > 280:
+            is_race_start = False
+            winning_color = turtles[i].pencolor()
+            if winning_color == user_bet:
+                print("You Won")
+            else:
+                print(f"{winning_color} is won")
+        turtles[i].forward(random.randint(0,10))
+
 
 
 screen.exitonclick()
