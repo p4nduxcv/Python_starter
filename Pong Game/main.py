3cc6db52fa00ae283746a569a577b68e078d75b5
Pandu <pandula95@gmail.com>
2025-07-04 15:18:40 +0530

    Pong Part 4 - Ball movement

--- Pong Game/main.py
+++ Pong Game/main.py
@@ -1,16 +1,21 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 screen = Screen()
 screen.setup(800, 600)
 screen.tracer(0)
+ball = Ball()
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
 is_game_one = True
 
 while is_game_one:
+    time.sleep(0.1)
     screen.update()
     screen.listen()
+    ball.move()
 
     screen.onkey(r_paddle.go_up, "Up")
     screen.onkey(r_paddle.go_down, "Down")
