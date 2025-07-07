48faa524a41d1e75f2782b6fcb5461bd7a9c1e11
Pandu <pandula95@gmail.com>
2025-07-07 20:26:22 +0530

    Crossing capstone : Create cars and move

--- Crossing Capstone/car_manager.py
+++ Crossing Capstone/car_manager.py
@@ -1,7 +1,35 @@
+from turtle import Turtle
+import random
+
+
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
 
 
 class CarManager:
-    pass
+    def __init__(self):
+        self.all_cars = []
+
+    def create_cars(self):
+        rad_int = random.randint(1,7)
+        if rad_int == 3:
+            new_car = Turtle()
+            new_car.shape("square")
+            new_car.shapesize(stretch_len=2, stretch_wid=1)
+            new_car.penup()
+            new_car.color(random.choice(COLORS))
+            random_y = random.randint(-250, 250 )
+            new_car.goto(300, random_y)
+            self.all_cars.append(new_car)
+        else:
+            pass
+
+    def move_car(self):
+        for car in self.all_cars:
+            car.backward(STARTING_MOVE_DISTANCE)
+
+
+
+
--- Crossing Capstone/main.py
+++ Crossing Capstone/main.py
@@ -10,11 +10,14 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 player = Player()
 screen.onkey(player.go_up, "Up")
+car_manager = CarManager()
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    car_manager.create_cars()
+    car_manager.move_car()
 
 
 
