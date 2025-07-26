d10a16de78ab76dbef2ef2d1ef3dbfe5c82c9eb5
Pandu <pandula95@gmail.com>
2025-07-26 21:09:03 +0530

    Day 26 - Dictionary comprehensions

--- Day 26/main.py
+++ Day 26/main.py
@@ -1,13 +1,26 @@
-numbers = [1,2,3,5]
-name = "Pandula"
+# numbers = [1,2,3,5]
+# name = "Pandula"
+#
+# new_numbers = [number+1 for number in numbers]
+# new_name_letter_list = [l for l in name]
+#
+# new_range = [n*2 for n in range(1,5)]
+#
+# names = ["Pandu", "Shaki", "Monohara", "Mandawala", "test", "ArR"]
+#
+# short_name = [name.capitalize() for name in names if len(name) > 5]
+#
+# print(short_name)
 
-new_numbers = [number+1 for number in numbers]
-new_name_letter_list = [l for l in name]
 
-new_range = [n*2 for n in range(1,5)]
 
-names = ["Pandu", "Shaki", "Monohara", "Mandawala", "test", "ArR"]
+# Dictionary Comprehension
+import random
+names = ["Alex", "Morrison", "Carlos", "Kyle", "kopr"]
 
-short_name = [name.capitalize() for name in names if len(name) > 5]
+student_score = {name:random.randint(1,100) for name in names}
+print(student_score)
 
-print(short_name)
+passed_students = {student:score for (student, score) in student_score.items() if score > 60}
+
+print(passed_students)
