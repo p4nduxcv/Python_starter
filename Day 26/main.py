336c6faab361602f3b643ee385a01a8266e1dcc3
Pandu <pandula95@gmail.com>
2025-07-29 12:20:07 +0530

    Day 26 - Dictionary comprehensions small coding test

--- Day 26/main.py
+++ Day 26/main.py
@@ -24,3 +24,12 @@ print(student_score)
 passed_students = {student:score for (student, score) in student_score.items() if score > 60}
 
 print(passed_students)
+
+#Coding test 2
+
+weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14, "Friday": 21, "Saturday": 22, "Sunday": 24}
+
+weather_f = {day: (temperature * (9/5)) + 32 for (day, temperature) in weather_c.items()}
+
+
+print(weather_f)
